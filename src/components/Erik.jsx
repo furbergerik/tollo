9027bc0481ae0df91a2b872bcf4028be9ea19c28
furbergerik@gmail.com <furbergerik@gmail.com>
2021-04-14 13:17:35 +0200

    Update Erik.jsx

--- src/components/Erik.jsx
+++ src/components/Erik.jsx
@@ -17,25 +17,52 @@ class Erik extends React.Component {
             <Bar
               data={{
                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-
                 datasets: [{
-                  data: [10, 3, 3, 5, 12, 3],
+                  label: 'Data set #1',
+                  data: [10, 10, 19, 5, 12, 3],
                   backgroundColor: [
-                    'red',
-                    'blue',
-                    'yellow',
-                    'green',
-                    'purple',
-                    'orange'
-                  ]
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+                },
+                {
+                  label: 'Data set #2',
+                  data: [101.5, 120, 129, 115, 112, 103],
+                }
 
-                }]
 
+                ],
               }}
-
 
               height={400}
               width={600}
+
+              options={{
+                maintainAspectRatio: false,
+                scales: {
+                  yAxes: [
+                    {
+                      ticks: {
+                        beginAtZero: true,
+
+                      }
+                    }
+                  ]
+                }
+              }}
             />
 
 
