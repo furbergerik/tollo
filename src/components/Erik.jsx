bb6359bac1f567bcef467580f06dc3b756696cf7
furbergerik@gmail.com <furbergerik@gmail.com>
2021-04-14 17:02:01 +0200

    piechart

--- src/components/Erik.jsx
+++ src/components/Erik.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import './Erik.css';
 import { Bar } from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
 
 class Erik extends React.Component {
   state = {
@@ -11,101 +12,101 @@ class Erik extends React.Component {
     return (
       <div class="container-fluid">
 
-        <div class="row">
+        <div className="row row1">
+          <div className="col-6">
+            <h1>Bar Chart</h1>
+          </div>
 
-          <div>
-            <Bar
+          <div className="col-6">
+            <h1 className="myBox">Doughnut Chart</h1>
+          </div>
+
+        </div>
+
+        <div class="row row2">
+          <div className="col-6">
+            <div>
+              <Bar
+                data={{
+                  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                  datasets: [{
+                    label: 'Data set #1',
+                    data: [10, 10, 19, 5, 12, 3],
+                    backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                  },
+                  {
+                    label: 'Data set #2',
+                    data: [101.5, 120, 129, 115, 112, 103],
+                  }
+
+
+                  ],
+                }}
+
+                options={{
+                  maintainAspectRatio: true,
+                  responsive: true,
+                  scales: {
+                    yAxes: [
+                      {
+                        ticks: {
+                          beginAtZero: true,
+                        }
+                      }
+                    ]
+                  }
+                }}
+              />
+
+
+            </div>
+
+          </div>
+
+          <div className="col-6 position-relative">
+            <div className="text-align-center align-self-center bg-primary" >hej</div>
+            <Doughnut
               data={{
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: ['Red', 'Blue', 'Yellow'],
                 datasets: [{
                   label: 'Data set #1',
-                  data: [10, 10, 19, 5, 12, 3],
+                  data: [15, 20, 7],
                   backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 206, 86, 0.2)'
                   ],
                   borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(255, 206, 86, 1)'
                   ],
                   borderWidth: 1
-                },
-                {
-                  label: 'Data set #2',
-                  data: [101.5, 120, 129, 115, 112, 103],
-                }
-
-
-                ],
+                }],
               }}
 
-              height={400}
-              width={600}
-
-              options={{
-                maintainAspectRatio: false,
-                scales: {
-                  yAxes: [
-                    {
-                      ticks: {
-                        beginAtZero: true,
-
-                      }
-                    }
-                  ]
-                }
-              }}
+              options={{}}
             />
-
-
           </div>
 
-
-
         </div>
-
-
-
-        <div class="row jumbotron" >
-
-          <div class="col-2"> Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Ullam, maiores enim!
-          Aliquid numquam exercitationem mollitia
-          corrupti accusantium saepe pariatur ratione.</div>
-
-          <div class="col-10"> Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Ullam, maiores enim!
-          Aliquid numquam exercitationem mollitia
-          corrupti accusantium saepe pariatur ratione.</div>
-
-        </div>
-
-        <div class="row">
-
-          <div class="col-2"> Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Ullam, maiores enim!
-          Aliquid numquam exercitationem mollitia
-          corrupti accusantium saepe pariatur ratione.</div>
-
-          <div class="col-10"> Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Ullam, maiores enim!
-          Aliquid numquam exercitationem mollitia
-          corrupti accusantium saepe pariatur ratione.</div>
-
-        </div>
-
-
-
       </div>
-
     )
   };
 }
