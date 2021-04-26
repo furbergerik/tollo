e123a55b13f6fff08fe5454af745421adb4862b6
furbergerik@gmail.com <furbergerik@gmail.com>
2021-04-26 16:58:50 +0200

    Data to graph in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Doughnut } from 'react-chartjs-2';
 import './Home.css';
 import CountUp from 'react-countup';
 import data from "../data";
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
 import { Bar } from 'react-chartjs-2';
 
-var totSalesList = [];
 
 function getDaylyData(year, month, dataType) {
   var salesData = data["totSales"];
@@ -20,13 +21,14 @@ function getDaylyData(year, month, dataType) {
 }
 
 function getMonthlyData(year, dataType) {
+  var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
   var listMonth = [];
   for (var i = 1; i < 13; i++) {
     var monthTot = sumArr(getDaylyData(year, i, dataType))
     listMonth.push(monthTot)
   }
-  console.log(listMonth, year)
-  // return listMonth
+  // console.log(listMonth, year)
+  return [listMonth, monthNames]
 }
 
 function sumArr(arr) {
@@ -37,64 +39,122 @@ function sumArr(arr) {
 }
 
 
-function Home() {
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      dates: []
+    }
+  }
 
-  return (
-    <div className="home">
-      <div className="container-fluid">
-        <div className="row colGrid">
-          {/* -------------col one------------ */}
-          <div className="col-xs-12 colGrid  col-md-4 bg-warning">
-            <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Pick year!
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#1" onClick={getMonthlyData(2018, "Total sales")}>2018</a>
-                <a class="dropdown-item" href="#2" onClick={getMonthlyData(2019, "Total sales")}>2019</a>
-                <a class="dropdown-item" href="#3" onClick={getMonthlyData(2020, "Total sales"), console.log('klickade 2020')}>2020</a>
+  handleSelect = (e) => {
+    const year = Number(e)
+    const [neededData, dates] = getMonthlyData(year, "Total sales")
+    let dateList = Object.keys(neededData);
+    var totSalesList = [];
+    for (var i in neededData) {
+      totSalesList.push(neededData[i])
+    }
+    console.log("handleSelect: ", totSalesList);
+
+    this.setState({
+      data: neededData,
+      dates: dates
+    },
+      () => {
+        console.log("STATE  ", this.state.data)
+      })
+  };
+
+
+  render() {
+    return (
+      <div className="home">
+        <div className="container-fluid">
+          <div className="row colGrid">
+            {/* -------------col one------------ */}
+            <div className="col-xs-12 colGrid  col-md-4 bg-warning">
+              <DropdownButton
+                alignRight
+                title="Select year"
+                id="dropdown-menu-align-right"
+                onSelect={this.handleSelect.bind(this)}
+              >
+                <Dropdown.Item eventKey="2018">2018</Dropdown.Item>
+                <Dropdown.Item eventKey="2019">2019</Dropdown.Item>
+                <Dropdown.Item eventKey="2020">2020</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item eventKey="some link">some link</Dropdown.Item>
+              </DropdownButton>
+
+              <CountUp
+                start={-875.039}
+                end={160527.012}
+                duration={2.75}
+                separator=" "
+                decimals={4}
+                decimal=","
+                prefix="EUR "
+                suffix=" left"
+                onEnd={() => console.log('Ended! 👏')}
+                onStart={() => console.log('Started! 💨')}
+              >
+                {({ countUpRef, start }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                    <button onClick={start}>Start</button>
+                  </div>
+                )}
+              </CountUp>
+            </div>
+
+            {/* -------------col two------------ */}
+            <div className="col-xs-12 col-md-8 bg-warning colGrid">
+
+              <div className="row bg-light col2">
+                <Bar
+                  data={{
+                    labels: this.state.dates,
+                    datasets: [{
+                      label: 'Data set #1',
+                      data: this.state.data,
+                      backgroundColor:
+                        'rgba(255, 99, 132, 0.2)',
+                      borderWidth: 1
+                    },
+                    ],
+                  }}
+
+                  options={{
+                    maintainAspectRatio: true,
+                    responsive: true,
+                    scales: {
+                      yAxes: [
+                        {
+                          ticks: {
+                            beginAtZero: true,
+                          }
+                        }
+                      ]
+                    }
+                  }}
+                />
               </div>
+              <div className="row bg-success col2">
+                row 2 col 2
             </div>
-            <CountUp
-              start={-875.039}
-              end={160527.012}
-              duration={2.75}
-              separator=" "
-              decimals={4}
-              decimal=","
-              prefix="EUR "
-              suffix=" left"
-              onEnd={() => console.log('Ended! 👏')}
-              onStart={() => console.log('Started! 💨')}
-            >
-              {({ countUpRef, start }) => (
-                <div>
-                  <span ref={countUpRef} />
-                  <button onClick={start}>Start</button>
-                </div>
-              )}
-            </CountUp>
-          </div>
 
-          {/* -------------col two------------ */}
-          <div className="col-xs-12 col-md-8 bg-warning colGrid">
+            </div>
 
-            <div className="row bg-secondary col2">
-              row 1 col 2
-            </div>
-            <div className="row bg-success col2">
-              row 2 col 2
-            </div>
+
+
 
           </div>
-
-
-
-
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Home;
