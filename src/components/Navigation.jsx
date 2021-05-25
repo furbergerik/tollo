efd4214ca054b7992f72921641910a01bbfa3d5c
asder256 <olle.kindvall@gmail.com>
2021-05-25 14:52:02 +0200

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ import './Navigation.css';
 
 function Navigation(props) {
   return (
-    <div className="navigation">
+    <div className="navigation ">
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">
