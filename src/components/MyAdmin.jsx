45fa4d0c7b26bd5f90777712881475710a67b14e
asder256 <olle.kindvall@gmail.com>
2021-05-25 14:04:08 +0200

    Update MyAdmin.jsx

--- src/components/MyAdmin.jsx
+++ src/components/MyAdmin.jsx
@@ -15,7 +15,7 @@ async function getUsers(department,store) {
   console.log(department);
   var token = (cookies.get('jwt')).key;
 
-  var fetchingFrom = `http://tollo.duckdns.org:61338/getUsersAdmin?store=${store}&department=${department}&token=${token}`
+  var fetchingFrom = `http://tollo.duckdns.org:61338/getUsersAdmin?store=${store}&department=${department}&allUsers=1&token=${token}`
 
   //var fetchingFrom = `http://192.168.0.111:61339/getUsersAdmin?store=${1}&department=${department}&token=${token}`
     const response = await fetch(fetchingFrom);
