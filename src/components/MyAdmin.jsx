05e91fd28d92026ce38148fd2ea18c14d1f3af14
asder256 <olle.kindvall@gmail.com>
2021-05-26 16:10:20 +0200

    hugo ?

--- src/components/MyAdmin.jsx
+++ src/components/MyAdmin.jsx
@@ -16,9 +16,10 @@ async function getUsers(department,store) {
   console.log(department);
   var token = (cookies.get('jwt')).key;
 
+ 
   var fetchingFrom = `http://tollo.duckdns.org:61338/getUsersAdmin?store=${store}&department=${department}&allUsers=1&token=${token}`
 
- // var fetchingFrom = `http://192.168.0.111:61339/getUsersAdmin?store=${1}&department=${department}&token=${token}`
+//  var fetchingFrom = `http://192.168.0.111:61339/getUsersAdmin?store=${1}&department=${department}&token=${token}`
     const response = await fetch(fetchingFrom);
     const setOfData = await response.json();
     const finalSet = setOfData.data;
@@ -431,21 +432,19 @@ class MyAdmin extends Component {
 
     else if(this.state.tab == "Settings"){
       message = 
+      <div class="container">
       <div className="mt-2 row ">
           
     
-        <div className="employee 1profileInfo shadow p-3 mb-5 rounded col-lg-3 offset-lg-2 mt-1" >
+        <div className="employee1 profileInfo shadow p-3 mb-5 rounded col-lg-4 col-md-12  " >
         {<UserInformation></UserInformation>}
         </div>
 
-<div>
-
-</div>
-<div className="employee1 col-lg-3">
+<div className="employee1 col-md-12  col-lg-4">
         {<Registration></Registration>}
    
         </div>
-        <div   className="employee1 employee  col-lg-3 overflow-auto shadow mb-5  p-3 rounded">
+        <div   className="employee1 employee  col-lg-4 col-md-12  overflow-auto shadow mb-5  p-3 rounded">
           <h1>Store employees:</h1> 
           <div className="input-group mb-3 ">
   <div className="input-group-prepend">
@@ -483,6 +482,7 @@ class MyAdmin extends Component {
     
         </div>
 
+  </div>
   </div>
     }
   
